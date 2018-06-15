c0bdd54e38744026d423a609382d34aa148a9f6e
Bernice Abreu <bernicemabreu@gmail.com>
2018-06-15 12:10:53 -0400

    installed mapview

--- components/MapPage.js
+++ components/MapPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, Button, Alert } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class MapPage extends React.Component {
   constructor(props) {
@@ -10,7 +11,14 @@ export default class MapPage extends React.Component {
   render() {
     return (
       <View>
-        <Text>HELLO WORLD</Text>
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
       </View>
     );
   }
