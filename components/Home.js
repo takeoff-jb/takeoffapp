905b5a23e407234abac7ecad53037cbb8fc3c1a3
Bernice Abreu <bernicemabreu@gmail.com>
2018-06-16 14:20:02 -0400

    fixed button

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity
+} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Check out my Home Screen!</Text>
-        <Button
+        <TouchableOpacity
           onPress={() =>
             this.props.navigation.navigate('ActivityForm', { hello: 'world' })
-          }
-          title="Start"
-        />
+          }>
+          <Image
+            style={styles.button}
+            source={require('../utils/title-image.png')}
+          />
+        </TouchableOpacity>
+        {/* <Image source={require('../utils/title-image.png')} /> */}
+        {/* <Button */}
+        {/* // style={styles.button}
+          // onPress={() =>
+          //   this.props.navigation.navigate('ActivityForm', { hello: 'world' })
+          // }
+        //   title="Let's Get Started"
+        //   color="red" */}
+        // />
       </View>
     );
   }
@@ -22,8 +40,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    textAlign: 'center',
+    color: 'red'
   }
+  // button: {
+  //   fontSize: 20,
+  //   fontWeight: 'bold'
+  // }
 });
 
 export default Home;
